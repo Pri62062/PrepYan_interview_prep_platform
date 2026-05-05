198ad26548bc134b190a44f57e18240adc3f6dd0
Priyanshu Kumar <kumarpriyanshu26139@gmail.com>
2026-05-05 18:36:16 +0530

    core congigfix

--- src/main/java/com/prep/config/CorsConfig.java
+++ src/main/java/com/prep/config/CorsConfig.java
@@ -2,8 +2,7 @@ package com.prep.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
 public class CorsConfig {
@@ -15,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOriginPatterns(
+                        .allowedOriginPatterns(   // ✅ IMPORTANT CHANGE
                                 "http://localhost:*",
                                 "http://127.0.0.1:*",
                                 "https://prepyan-app.netlify.app",
