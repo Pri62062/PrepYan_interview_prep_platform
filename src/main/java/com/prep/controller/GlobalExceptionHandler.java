31160e4c2efd3de29469e22eb8815988a33f70d6
Priyanshu Kumar <kumarpriyanshu26139@gmail.com>
2026-05-05 18:23:53 +0530

    fix: config cleanup + view resolver issue

--- src/main/java/com/prep/controller/GlobalExceptionHandler.java
+++ src/main/java/com/prep/controller/GlobalExceptionHandler.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.HashMap;
 import java.util.Map;
 
-@ControllerAdvice
+@RestControllerAdvice   // ✅ FIXED (VERY IMPORTANT)
 public class GlobalExceptionHandler {
 
     // 🔥 Handle RuntimeException
